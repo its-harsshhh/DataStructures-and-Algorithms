5dfb01ee569db744cbc8cbbd3eaabfe9d9bf61cf
Harsh <harshpal653@gmail.com>
2022-06-17 18:37:20 +0530

    For+While+Dowhile

---
+++ Loops Basics/basics_loops.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {  /*int n;
+    cin>>n;
+    for(int a=1;a<=n;a++)          //print hello as much as you want [for loop]
+    {
+    cout<<"hello"<<endl;
+    }*/
+
+
+
+    /*int n;
+    cin>>n;
+    while(n<=5)
+    {
+    cout<<"hello"<<endl;            //infinite loop [while loop]
+    }*/
+
+
+
+    /*int n;
+    cin>>n;
+    do{
+        cout<<"hello"<<endl;
+        cin>>n;                         //positive value -> print hello
+    }while(n>0);*/                     //negative value ->exit             [do-while loop]
+
+
+
+    /*int n;
+    cin>>n;                             //positive value -> print hello
+    while(n>0)                         //negative value ->exit             [while loop]   
+    {
+    cout<<"hello"<<endl;
+    cin>>n;
+    }  */
+return 0;
+ }
